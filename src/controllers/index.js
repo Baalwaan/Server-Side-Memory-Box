a34dc559328f8f7f1c34416806294f1d1b512e81
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-09-27 12:38:21 +0100

    build db

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,4 +4,8 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.send('Server side says hello world'));
 
+router.get('/hi', (req, res) =>
+  res.send('hey react, heroku received your request')
+);
+
 module.exports = router;
