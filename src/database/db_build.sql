88375e6cdd742aa0bb7d4158c5d3bd7ab850e42c
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-09-29 23:19:54 +0100

    build diary databse

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -1,15 +1,14 @@
 BEGIN;
 
-DROP TABLE IF EXISTS gym CASCADE;
+DROP TABLE IF EXISTS diary CASCADE;
 
-create table gym (
+create table diary (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
-	gym_name VARCHAR(100) NOT NULL,
-	workout_duration VARCHAR(20) NOT NULL,
-    favourite_workout VARCHAR(50) NOT NULL,
+	log_type VARCHAR(20) NOT NULL,
+	message VARCHAR(250) NOT NULL,
 	time_stamp VARCHAR(100)
 );
-insert into gym (name, gym_name, workout_duration, favourite_workout, time_stamp) values ('Merola', 'Pure Gym', '45-60 Min', 'Chest Press', now()),('Mesut', 'Northampton leisure center', '30-45 Min', 'Arms', now()),('Abid', 'Virgin Gym', 'over 1.5Hr', 'Squats', now());
+insert into diary (name, log_type, message, time_stamp) values ('Burhan', 'Challenge', 'Today I built this database for a daily memory logge and I am proud of it', now());
 
 COMMIT;
