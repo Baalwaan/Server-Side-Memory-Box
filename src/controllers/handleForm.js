dcd9641564cb4a12fe8d2e933ab5ba99db2b841e
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-09-30 16:57:43 +0100

    Update handleForm.js

--- src/controllers/handleForm.js
+++ src/controllers/handleForm.js
@@ -1,6 +1,6 @@
 const handleForm = (req, res) => {
   console.log('this is your form: ', req.body);
-  res.send('heres your resposne ', req.body);
+  res.send(req.body);
 };
 
 module.exports = handleForm;
