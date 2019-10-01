f5fed09c08aa8fa7eac9f647d1a74341509d5820
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-10-01 15:52:13 +0100

    create db postquery

--- src/controllers/handleForm.js
+++ src/controllers/handleForm.js
@@ -1,6 +1,15 @@
+const postFormQuery = require('../database/queries/postForm');
+
 const handleForm = (req, res) => {
-  console.log('this is your form: ', req.body);
-  res.send(JSON.stringify({'message': 'heroku says hi'}));
+  // console.log('this is your form: ', req.body);
+  const name = 'Burhan';
+  const messageType = 'Challenge';
+  const message = 'Hi, heroku is king';
+  // const { name, messageType, message } = req.body;
+  console.log('deconstructed', name, messageType, message);
+  postFormQuery(name, messageType, message)
+    .then(dbRes => res.send(JSON.stringify(dbRes)))
+    .catch(e => console.log('error: ', e));
 };
 
 module.exports = handleForm;
