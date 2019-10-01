95d3007e6331f277cc6cd9490d52b303bb49ed5e
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-10-01 12:26:51 +0100

    Update handleForm.js

--- src/controllers/handleForm.js
+++ src/controllers/handleForm.js
@@ -1,6 +1,6 @@
 const handleForm = (req, res) => {
   console.log('this is your form: ', req.body);
-  res.send(JSON.stringify(req.body));
+  res.send(JSON.stringify({'message': 'heroku says hi'}));
 };
 
 module.exports = handleForm;
