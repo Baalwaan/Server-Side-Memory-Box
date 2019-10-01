a97cbff65317cbce58f53f56a210acdfd9fad3d2
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-10-01 15:55:09 +0100

    req.body deconstructring form data from client

--- src/controllers/handleForm.js
+++ src/controllers/handleForm.js
@@ -1,11 +1,7 @@
 const postFormQuery = require('../database/queries/postForm');
 
 const handleForm = (req, res) => {
-  // console.log('this is your form: ', req.body);
-  const name = 'Burhan';
-  const messageType = 'Challenge';
-  const message = 'Hi, heroku is king';
-  // const { name, messageType, message } = req.body;
+  const { name, messageType, message } = req.body;
   console.log('deconstructed', name, messageType, message);
   postFormQuery(name, messageType, message)
     .then(dbRes => res.send(JSON.stringify(dbRes)))
