190fcd3f52f4698d99fb8930983891791a497eae
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-10-02 19:08:30 +0100

    remove unwanted router endpoints

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,11 +6,6 @@ const handleGetMemories = require('./handleGetMemories');
 const router = express.Router();
 
 router.get('/', (req, res) => res.send('Server side says hello world'));
-
-router.get('/hi', (req, res) =>
-  res.send({ server: 'hey react, heroku received your request' })
-);
-
 router.post('/submit-post', handleForm);
 router.post('/get-memories', handleGetMemories);
 
