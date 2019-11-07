c86d48af867d13a04920534b739a5b94df065301
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-11-07 23:15:12 +0000

    Update postForm.js

--- src/database/queries/postForm.js
+++ src/database/queries/postForm.js
@@ -3,7 +3,7 @@ const db = require('../db_connection');
 const postQuery = (name, log_type, message) => {
   return db
     .query(
-      `INSERT INTO diary (name, log_type, message, time_stamp) VALUES ($1, $2, $3, TO_CHAR(NOW() :: DATE, 'day dd Mon, yyyy')) RETURNING *`,
+      `INSERT INTO diary (name, log_type, message, time_stamp) VALUES ($1, $2, $3, TO_CHAR(NOW() :: DATE, 'Day dd Mon, yyyy')) RETURNING *`,
       [name, log_type, message]
     )
     .then(dbRes => dbRes.rows)
