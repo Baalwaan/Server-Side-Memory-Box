44d4ea003aab3503999ea2f247ce0ca19724e839
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-11-07 16:27:09 +0000

    Update getMemories.js

--- src/database/queries/getMemories.js
+++ src/database/queries/getMemories.js
@@ -2,7 +2,7 @@ const db = require('../db_connection');
 
 const getMemoriesQuery = name => {
   return db
-    .query('SELECT * FROM diary WHERE (name)=$1', [name])
+    .query('SELECT * FROM diary WHERE (name)=$1','SELECT time_stamp, TO_CHAR(timestamp, 'day dd Mon, yyyy') FROM diary WHERE (name)=$1' [name])
     .then(dbRes => dbRes.rows)
     .catch(err => console.log('Error: ', err));
 };
